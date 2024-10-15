f3fda5e1910b2d4856f22216b7f05b6e4fa88570
Antho137 <antoniosala7@gmail.com>
2024-10-15 02:17:38 +0100

    fix initial app

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/dream-life/">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
